ddf4fa155ddf1cadaad7580a8421a10218cdeb87
ewerton <ewertonruiz@gmail.com>
2022-08-09 23:20:54 -0500

    #4 Execução de novo calculo sem executar aplicação

--- calculadora.py
+++ calculadora.py
@@ -47,5 +47,38 @@ else:
     print (' you have not typed a valid operator, please run the program again.')
 
 
+def calculate ():
+    Operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+
+    #Add again() function to calcule () function
+    again() def again():
+        calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+calculate()
+
+
+
+
+
+
+
+
+
+    
 
 
